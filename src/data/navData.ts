200e5f9298a23c0a3d666a0eac25265f633ace94
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2023-09-17 18:36:00 +0100

    Replace navData.js with a .ts file - And add other nav destinations

---
+++ src/data/navData.ts
@@ -0,0 +1,20 @@
+const navData = [
+  {
+    name: "Articles",
+    path: "/blog",
+  },
+  {
+    name: "Projects",
+    path: "/projects",
+  },
+  {
+    name: "Photography",
+    path: "/gallery",
+  },
+  {
+    name: "My Setup",
+    path: "/setup",
+  },
+];
+
+export default navData;
