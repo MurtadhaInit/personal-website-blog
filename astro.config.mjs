6521dfb2a24c8d4d02c496e3eca50315211532d4
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2023-09-17 18:19:33 +0100

    Add tailwindcss integration - And remove the global.css file from the tutorial template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 
+// integrations
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://murtadha.net",
+  // experimental status is set for the Assets API (optimised images): https://docs.astro.build/en/guides/assets/
   experimental: {
     assets: true,
   },
-  integrations: [sitemap()],
+  integrations: [sitemap(), tailwind()],
 });
