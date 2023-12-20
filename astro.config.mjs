79f47215bea82ad59f8216b3a7d2c6ee5268eb18
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2023-12-20 13:08:34 +0000

    Upgraded to Astro 4.0 along with all plugins and integrations - Removed the experimental flag from astro config for Assets API - astro check command is now a separate package, installed a dev dep - installed typescript as a dev dep - CollectionEntry now has to be imported as type specifically - Everything builds and runs normally
    UPGRADES:
    - rss integration 2.4.4 -> 4.0.1
    - sitemap integration 2.0.1 -> 3.0.3
    - tailwind integration 4.0.0 -> 5.0.3
    - astro 2.9.3 -> 4.0.6
    - tailwindcss 3.3.3 -> 3.4.0
    - prettier 3.1.0 -> 3.1.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://murtadha.net",
-  // experimental status is set for the Assets API (optimised images): https://docs.astro.build/en/guides/assets/
-  experimental: {
-    assets: true,
-  },
   integrations: [sitemap(), tailwind()],
 });
