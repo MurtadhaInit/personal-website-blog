d22708f21365a60c805cd764176aa73c29317a83
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2024-01-16 16:31:22 +0000

    Transferred Link to components and out of ui and extracted Header, Footer, and navData out

---
+++ src/data/navData.ts
@@ -0,0 +1,22 @@
+export const navData = [
+  {
+    label: "Articles",
+    path: "/blog",
+    footerNav: true,
+  },
+  {
+    label: "Projects",
+    path: "/projects",
+    footerNav: true,
+  },
+  {
+    label: "Photography",
+    path: "/gallery",
+    footerNav: false,
+  },
+  {
+    label: "My Setup",
+    path: "/setup",
+    footerNav: true,
+  },
+];
