b5eb8f638bee3873503952532d6118b1d0be0874
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2024-01-28 00:27:19 +0000

    After trying out NextUI and Shadcn/ui, we settled on simple custom implementation without either - Installed and added React and will use it later on for interactive components - changed the tailwind config file to be mjs - Defined a basic theme set of colours - decided to use a global.css file that is loaded in the base layout to allow for customising tailwind in there

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from "astro/config";
 
 // integrations
+import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://murtadha.net",
-  integrations: [sitemap(), tailwind()],
+  integrations: [
+    sitemap(),
+    tailwind({
+      // disable injecting a basic `base.css` import on every page
+      // use a single global CSS file instead (for configuration)
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
 });
