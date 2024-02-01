3bf4321e490bb10af2749c003d8aad9171f795e1
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2024-02-01 21:46:10 +0000

    Fixed all issues with dark mode toggle

--- public/toggleTheme.js
+++ public/toggleTheme.js
@@ -1,37 +1,36 @@
-const currentTheme = localStorage.getItem("theme");
-
 function getThemePref() {
   // get current theme, if set
-  if (currentTheme) return currentTheme;
+  const setTheme = localStorage.getItem("theme");
+  if (setTheme) return setTheme;
 
   // or get system preference otherwise (and store it in local storage)
-  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    localStorage.setItem("theme", "dark");
-    return "dark";
-  }
-  localStorage.setItem("theme", "light");
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) return "dark";
   return "light";
 }
 
-let theme = getThemePref();
-
-function reflectThemePref() {
+function reflectThemePref(theme) {
   theme === "dark"
     ? document.documentElement.classList.add("dark")
     : document.documentElement.classList.remove("dark");
 
   const toggleThemeButton = document.getElementById("theme-btn");
-  toggleThemeButton?.setAttribute("aria-label", theme + " theme");
+  toggleThemeButton?.setAttribute(
+    "aria-label",
+    `Switch to ${theme === "dark" ? "light" : "dark"} theme`,
+  );
 }
 
-// set the specified theme early on
-reflectThemePref();
+reflectThemePref(getThemePref());
 
 window.onload = () => {
+  // set the specified theme early on
+  reflectThemePref(getThemePref());
+
   // toggle theme manually
   document.getElementById("theme-btn")?.addEventListener("click", () => {
-    theme = theme === "dark" ? "light" : "dark";
-    reflectThemePref();
+    const theme = getThemePref() === "dark" ? "light" : "dark";
+    localStorage.setItem("theme", theme);
+    reflectThemePref(theme);
   });
 };
 
@@ -39,6 +38,8 @@ window.onload = () => {
 window
   .matchMedia("(prefers-color-scheme: dark)")
   .addEventListener("change", ({ matches: isDark }) => {
-    theme = isDark ? "dark" : "light";
-    reflectThemePref();
+    const theme = isDark ? "dark" : "light";
+    const setTheme = localStorage.getItem("theme");
+    // if theme was not manually set, reflect change
+    if (!setTheme) reflectThemePref(theme);
   });
