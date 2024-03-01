7943b24e75d9f008d36c50cabb39a2717e211a80
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2024-03-01 00:05:18 +0000

    The first section of the index page is now mostly implemented - Added a new file in the data directory with contact links and labels. - The main tag is now inside the index page rather than the layout. - There still appears to be an issue of sizing with grid cells.

---
+++ src/data/contactLinks.ts
@@ -0,0 +1,23 @@
+export const contactLinks = [
+  {
+    label: "X",
+    path: "https://twitter.com/MurtadhMA",
+    icon: "mdi:twitter",
+  },
+  {
+    label: "GitHub",
+    path: "https://github.com/MurtadhaInit",
+    icon: "mdi:github",
+  },
+  {
+    label: "LinkedIn",
+    path: "",
+    icon: "mdi:linkedin",
+  },
+];
+
+export const contactEmail = {
+  label: "Get in touch",
+  path: "murtadha@murtadha.com",
+  icon: "mdi:email",
+};
