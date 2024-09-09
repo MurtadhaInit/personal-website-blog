d2db97365a83b085f05d6b3ef5ffa45871078d81
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2024-09-10 02:24:14 +0300

    Fixed and refactored the grid in the 1st section in the index page and added a technologies subsection - Installed devicon-plain icons set for the new subsection - Technologies I'm familiar with is now put in a file in @data - Removed the width from the image subsection

---
+++ src/data/technologies.ts
@@ -0,0 +1,44 @@
+export const technologies = [
+  {
+    icon: "devicon-plain:python",
+  },
+  {
+    icon: "devicon-plain:fastapi",
+  },
+  {
+    icon: "devicon-plain:jupyter-wordmark",
+  },
+  {
+    icon: "devicon-plain:javascript",
+  },
+  {
+    icon: "devicon-plain:typescript",
+  },
+  {
+    icon: "devicon-plain:astro",
+  },
+  {
+    icon: "mdi:tailwind",
+  },
+  {
+    icon: "devicon-plain:ansible",
+  },
+  {
+    icon: "devicon-plain:docker",
+  },
+  {
+    icon: "devicon-plain:bash",
+  },
+  {
+    icon: "devicon-plain:figma",
+  },
+  {
+    icon: "devicon-plain:githubactions",
+  },
+  {
+    icon: "devicon-plain:linux",
+  },
+  {
+    icon: "devicon-plain:java",
+  },
+];
