1350a7f6963ad3afc220d8e2633479d539c24a88
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2024-10-23 02:06:49 +0300

    Trying out deployment to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://murtadha.net",
+  site: "https://murtadha.site",
   integrations: [
     icon({
       include: {
