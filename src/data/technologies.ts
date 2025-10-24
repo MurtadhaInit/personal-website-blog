5d2efd32ff35387b2b4fa7ddcc57a1bb8d09abbc
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2025-10-24 15:06:10 +0300

    Adjusted 'technologies' as follows: removed the Notion icon and added ones for nix, sql, and svelte

--- src/data/technologies.ts
+++ src/data/technologies.ts
@@ -1,53 +1,23 @@
 export const technologies = [
-  {
-    icon: "devicon-plain:linux",
-  },
-  {
-    icon: "devicon-plain:ansible",
-  },
-  {
-    icon: "devicon-plain:terraform",
-  },
-  {
-    icon: "devicon-plain:docker",
-  },
-  {
-    icon: "simple-icons:nushell",
-  },
-  {
-    icon: "devicon-plain:typescript",
-  },
-  {
-    icon: "devicon-plain:astro",
-  },
-  {
-    icon: "mdi:tailwind",
-  },
-  {
-    icon: "devicon-plain:python",
-  },
-  {
-    icon: "devicon-plain:jupyter-wordmark",
-  },
-  {
-    icon: "devicon-plain:fastapi",
-  },
-  {
-    icon: "devicon-plain:figma",
-  },
-  {
-    icon: "devicon-plain:notion",
-  },
+  { icon: "devicon-plain:nixos" },
+  { icon: "devicon-plain:linux" },
+  { icon: "devicon-plain:ansible" },
+  { icon: "devicon-plain:terraform" },
+  { icon: "devicon-plain:docker" },
+  { icon: "simple-icons:nushell" },
+  { icon: "devicon-plain:typescript" },
+  { icon: "devicon-plain:astro" },
+  { icon: "mdi:tailwind" },
+  { icon: "devicon-plain:python" },
+  { icon: "devicon-plain:jupyter-wordmark" },
+  { icon: "devicon-plain:fastapi" },
+  { icon: "devicon-plain:figma" },
+  { icon: "devicon-plain:svelte" },
   // {
   //   icon: "devicon-plain:bash",
   // },
-  {
-    icon: "devicon-plain:githubactions",
-  },
-  {
-    icon: "devicon-plain:java",
-  },
-  {
-    icon: "devicon-plain:go",
-  },
+  { icon: "devicon-plain:githubactions" },
+  { icon: "devicon-plain:java" },
+  { icon: "devicon-plain:go" },
+  { icon: "devicon-plain:azuresqldatabase" },
 ];
