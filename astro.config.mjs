7869cf511d118c19cc6255604aaad6c8be04493c
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2025-10-25 20:25:15 +0300

    Change the custom domain for the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://murtadha.site",
+  site: "https://murtadha.dev",
 
   integrations: [
     icon({
